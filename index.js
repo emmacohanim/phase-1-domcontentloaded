d14da4ab5b95dcdf3d683b0214026a56f7bcad58
Emma Cohanim <emmacohanim@gmail.com>
2022-07-21 09:50:51 -0700

    commit tree bf00b40437637766986695595311996646c9445b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",
+function() {
+    console.log("The DOM has loaded")
+
+// const text = document.getElementById("text")
+// text.textContent = "This is really cool!"
+let p = document.getElementById("text")
+p.addEventListener("click", function() {
+    console.log("Clicked!")
+})
+p.textContent = "This is really cool!"
+})
+
+//Putting the script in the head
+//-speed up page loading time bc request to fetch javascript comes sooner
